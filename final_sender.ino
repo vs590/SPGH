db0a36e8f92e5e89a6ce8128bc8f823df817ce6b
Vyom Saxena <vs590@snu.edu.in>
2017-04-23 14:45:26 +0530

    Add files via upload

---
+++ final_sender.ino
@@ -0,0 +1,81 @@
+int i=1;
+volatile int ldr, ldr1=-7,ldr2=-2, pir, pir1=-8, pir2=-3,ir, ir1=-9, ir2=-4,c=0;
+void setup() {
+  
+Serial.begin(38400);
+
+
+}
+
+void loop() 
+{
+  
+  // put your main code here, to run repeatedly:
+  int ldr= analogRead(A0);
+  int pir= digitalRead(7);
+  int ir= digitalRead(6); 
+ 
+if(ldr>0 && ldr<164)
+  {
+    ldr1 = 1;
+  }
+  else if(ldr>163 && ldr<300)
+  {
+    ldr1 = 2;
+   // analogWrite(A3,63);
+  }
+  else if(ldr>299 && ldr<500)
+  {
+    ldr1 = 3;
+    //analogWrite(A3,126);
+    }
+  else if(ldr>499 && ldr<750)
+  {
+    ldr1 = 4;
+    //analogWrite(A3,190);
+  }
+  else if(ldr>749 && ldr<1025)
+  {
+    ldr1 = 5;
+  }
+
+  if(ldr1!=ldr2){
+    Serial.println(ldr1);
+    ldr2=ldr1;
+  }
+ 
+  
+  if(pir==HIGH)
+  {
+    pir1=6;
+  }
+  else if(pir==LOW)
+  {
+    pir1=7;
+  }
+  
+   if(pir1!=pir2){
+    Serial.println(pir1);
+    pir2=pir1;
+  }
+  if(ir==HIGH)
+  {
+     c++;
+  }
+  else if (ir==LOW)
+  {
+     c=0;
+  }
+  
+   if(c>=11){
+    ir1=8;}else ir1=9;
+    
+ if(ir1!=ir2){   
+    Serial.println(ir1);
+    ir2=ir1;
+ }
+ 
+  delay(500);
+ 
+  }
+
---
+++ receiver.ino
@@ -0,0 +1,53 @@
+int val;
+int tempPin = A8;
+int tempLed = 5;
+int ldrLed = 6;
+int pirLed = 7;
+int tankLed = 8;
+int rec;
+void setup()
+{
+Serial.begin(38400);
+pinMode(tempPin,INPUT);
+pinMode(tempLed,OUTPUT);
+pinMode(ldrLed,OUTPUT);
+pinMode(tankLed,OUTPUT);
+pinMode(pirLed,OUTPUT);
+}
+void loop()
+{
+val = analogRead(tempPin);
+float mv = ( val/1024.0)*5000; 
+float cel = mv/10;
+//float farh = (cel*9)/5 + 32;
+if (Serial.available() > 0){
+rec = Serial.read();
+}
+if (rec == '1'){
+   analogWrite(ldrLed, 0);
+} else if(rec == '2'){
+  analogWrite(ldrLed, 63);
+} else if(rec == '3'){
+  analogWrite(ldrLed, 127);
+} else if(rec == '4'){
+  analogWrite(ldrLed, 190);
+}else if(rec == '5'){
+  analogWrite(ldrLed, 255);
+}else if(rec == '6'){
+  digitalWrite(pirLed, HIGH);
+}else if(rec == '7'){
+  digitalWrite(pirLed, LOW);
+}else if(rec == '8'){
+  digitalWrite(tankLed, HIGH);
+}else if(rec == '9'){
+  digitalWrite(tankLed, LOW);
+}
+
+if(cel >  25){
+  digitalWrite(tempLed, 1);
+}else{
+  digitalWrite(tempLed, 0);
+}
+}
+
+
